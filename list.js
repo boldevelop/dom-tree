d9051ba217a30cdbec414183970a6f7a56783bef
Владислав Болдаков <15webboldakov@gmail.com>
2019-12-10 03:23:36 +0700

    feat: add insert element in list by pos

--- list.js
+++ list.js
@@ -32,20 +32,31 @@ const _checkList = (list) => {
  * @param {function:list} list - список.
  * @returns {function:list}
  */
-export const cons = (element, list) => {
+export const _cons = (element, list) => {
     _checkList(list);
     return pair.cons(element, list);
 };
 
-export const consPos = (element, list, pos) => {
+/**
+ * reverse list
+ * @param {*} element - вставляемый элемент.
+ * @param {function:list} list - список.
+ * @param {number} pos - позиция.
+ * @returns {function:list}
+ */
+export const cons = (element, list, pos = 0) => {
     _checkList(list);
     const lengthList = length(list);
-    return reduce(list, (a, e, i) => {
-        if (i === pos) {
+    if (pos === 0) {
+        return _cons(element, list);
+    }
+    const posReplace = pos > lengthList ? lengthList - 1 : pos;
+    return reverse(reduce(list, (a, e, i) => {
+        if (i === posReplace) {
             return cons(e, cons(element, a));
         }
-        return cons(e, a);
-    }, l());
+        return pair.cons(e, a);
+    }, null));
 };
 
 /**
@@ -89,6 +100,16 @@ export const isEmpty = list => {
 };
 
 /**
+ * reverse list
+ * @param {function:list} list - список.
+ * @returns {function:list}
+ */
+export const reverse = (list) => {
+    _checkList(list);
+    return reduce(list, (a, e) => cons(e, a), null)
+};
+
+/**
  * reduce for the list
  * @param {function:list} list - список.
  * @param {function} reducer - функция редьюсер.
@@ -129,31 +150,29 @@ export const has = (list, elem) => {
  * @param {function:list} list - список.
  * @returns {string}
  */
-// todo: оставить логику только для списков
 export const toString = (list) => {
     _checkList(list);
     const strOfList = reduce(list, (a, e) => {
         let str = '';
         if (isList(e)) {
-            str = `, ${toString(e)}`;
+            str = toString(e);
         } else {
-            let string = '';
             if (typeof e === 'object') {
-                string = JSON.stringify(e, null, 2);
+                str = JSON.stringify(e, null, 2);
             } else {
-                string = String(e);
+                str = String(e);
             }
-            str = a ? `, ${string}` : string;
         }
+        str = a ? `, ${str}` : str;
         return a + str;
     }, '');
     return `(${strOfList})`;
 };
 
 // todo: need testing
-const numberLists = l(1, 2,{name: 'll', cl: '213'}, l('qwe', 'rty'));
+const numberLists = l(1, 2, 3, l('qwe', 'rty'));
 const res = reduce(numberLists, (e, a) => a + e, 0);
 const len = length(numberLists);
 const addedL = cons(2, numberLists);
 console.log(toString(numberLists));
-console.log(toString(consPos('start', numberLists, 3)));
+console.log(toString(cons('start', numberLists, 3)));
